12b312456513bc81850e2a120400c03951e471dc
nicroxio <ferdi@beacroft-mitchell.co.uk>
2023-04-15 16:16:34 +0000

    put the propper thingy in

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 #Litteraly just to download pwninit and install gef, if the gef install doesn't work, install manually using https://github.com/hugsy/gef
 
-git clone https://github.com/io12/pwninit.git
+wget https://github.com/io12/pwninit/releases/download/3.3.0/pwninit
 bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
